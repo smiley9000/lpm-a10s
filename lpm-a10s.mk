10869ad3dc3d2b9db0c7d70e844fea45438ada78
Smiley <59495159+smiley9000@users.noreply.github.com>
2025-02-05 14:32:06 +0530

    initial

---
+++ lpm-a10s.mk
@@ -0,0 +1,30 @@
+# Automatically generated file. DO NOT MODIFY
+#
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/samsung/lpm
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/lpm/proprietary/bin/lpm:$(TARGET_COPY_OUT_SYSTEM)/bin/lpm \
+    vendor/samsung/lpm/proprietary/bin/kpoc_charger:$(TARGET_COPY_OUT_SYSTEM)/bin/kpoc_charger \
+    vendor/samsung/lpm/proprietary/etc/init/lpm.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/lpm.rc \
+    vendor/samsung/lpm/proprietary/etc/init/kpoc_charger.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/kpoc_charger.rc \
+    vendor/samsung/lpm/proprietary/lib64/libmaet.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmaet.so \
+    vendor/samsung/lpm/proprietary/lib64/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libshowlogo.so \
+    vendor/samsung/lpm/proprietary/lib64/libsxqk_skia.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsxqk_skia.so \
+    vendor/samsung/lpm/proprietary/media/battery_error.spi:$(TARGET_COPY_OUT_SYSTEM)/media/battery_error.spi \
+    vendor/samsung/lpm/proprietary/media/battery_low.spi:$(TARGET_COPY_OUT_SYSTEM)/media/battery_low.spi \
+    vendor/samsung/lpm/proprietary/media/battery_temperature_error.spi:$(TARGET_COPY_OUT_SYSTEM)/media/battery_temperature_error.spi \
+    vendor/samsung/lpm/proprietary/media/battery_temperature_limit.spi:$(TARGET_COPY_OUT_SYSTEM)/media/battery_temperature_limit.spi \
+    vendor/samsung/lpm/proprietary/media/battery_water_usb.spi:$(TARGET_COPY_OUT_SYSTEM)/media/battery_water_usb.spi \
+    vendor/samsung/lpm/proprietary/media/charging_New_Fast.spi:$(TARGET_COPY_OUT_SYSTEM)/media/charging_New_Fast.spi \
+    vendor/samsung/lpm/proprietary/media/charging_New_Normal.spi:$(TARGET_COPY_OUT_SYSTEM)/media/charging_New_Normal.spi \
+    vendor/samsung/lpm/proprietary/media/dock_error_usb.spi:$(TARGET_COPY_OUT_SYSTEM)/media/dock_error_usb.spi \
+    vendor/samsung/lpm/proprietary/media/incomplete_connect.spi:$(TARGET_COPY_OUT_SYSTEM)/media/incomplete_connect.spi \
+    vendor/samsung/lpm/proprietary/media/lcd_density.txt:$(TARGET_COPY_OUT_SYSTEM)/media/lcd_density.txt \
+    vendor/samsung/lpm/proprietary/media/frc_forced_params.ini:$(TARGET_COPY_OUT_SYSTEM)/media/frc_forced_params.ini \
+    vendor/samsung/lpm/proprietary/media/percentage.spi:$(TARGET_COPY_OUT_SYSTEM)/media/percentage.spi \
+    vendor/samsung/lpm/proprietary/media/safety_timer_usb.spi:$(TARGET_COPY_OUT_SYSTEM)/media/safety_timer_usb.spi \
+    vendor/samsung/lpm/proprietary/media/slow_charging_usb.spi:$(TARGET_COPY_OUT_SYSTEM)/media/slow_charging_usb.spi \
+    vendor/samsung/lpm/proprietary/media/temperature_limit_usb.spi:$(TARGET_COPY_OUT_SYSTEM)/media/temperature_limit_usb.spi \
+    vendor/samsung/lpm/proprietary/media/water_protection_usb.spi:$(TARGET_COPY_OUT_SYSTEM)/media/water_protection_usb.spi 
+
